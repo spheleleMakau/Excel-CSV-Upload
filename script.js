6c70c0e906ae35642700ca3b50b4dee85f55d140
spheleleMakau <missmakau9@gmail.com>
2024-05-08 22:37:35 +0200

    Added JavaScript code to create a new instance of FileReader to read the selected file content. When the file is loaded successfully, the data is retrieved as a Uint8Array and logged to the console for debugging purposes.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.getElementById('fileInput');
 const uploadButton = document.getElementById('uploadButton');
+const tableContainer = document.getElementById('tableContainer')
 
 // Event listener for file upload button
 uploadButton.addEventListener('click', () => {
@@ -13,5 +14,19 @@ uploadButton.addEventListener('click', () => {
 
     // see the file properties on console
     console.log(file)
+
+    //new instance of FileReader to read the file content
+    const reader = new FileReader();
+
+    // When the file is loaded successfully
+    reader.onload = (e) => {
+        // Get the file data as a Uint8Array and log the Uint8Array data to the console
+        const data = new Uint8Array(e.target.result);
+        console.log(data);
+        };
+        
+    // Start reading the file as an ArrayBuffer
+    reader.readAsArrayBuffer(file);
+
 
 });
